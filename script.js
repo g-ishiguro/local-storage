21e5f18e152779ad5e897e3153b21f230a7470c3
g-ishiguro <genki.i0918@gmail.com>
2018-03-10 14:08:56 +0900

    2018.3.10

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // var text = document.getElementById('text')
 // var copyBtn = document.getElementById('copy-btn')
-// // var resultText = document.getElementById('result-text')
+// var resultText = document.getElementById('result-text')
 // var resultArea = document.getElementById('result-area')
 // console.dir(text);
 //
@@ -23,36 +23,97 @@
 var input = document.getElementById('title');
 var textarea = document.getElementById('content');
 var addBtn = document.getElementById('add-btn');
+var removeBtn = document.getElementById('remove-btn');
 var toDoArea =  document.getElementById('to-do-area');
 var toDoItems = [];
-addBtn.addEventListener('click', function () {
-  //ToDoの表示
+
+function addToDo() {
   var title = input.value;
   var content = textarea.value;
   var toDoItem = {
     title: title,
     content: content,
   };
-  toDoItems.push(toDoItem);
+
+saveLocalStorage(toDoItem);
+displayToDo(toDoItem);
+  //入力エリアを空にする
+  input.value = "";
+  textarea.value = "";
+};
+
+function saveLocalStorage(toDoObj) {
+  toDoItems.push(toDoObj);
   //JSONに変換
   var data = JSON.stringify(toDoItems)
+  //保存
   localStorage.setItem('todo', data);
+}
 
-  displayToDo(title, content);
-});
+addBtn.addEventListener('click', addToDo);
 
-
-function displayToDo(ttl, cnt) {
+function displayToDo(toDoObj) {
   //h2、p追加
   var h2 = document.createElement('h2');
   var p =  document.createElement('p');
+  var deleteBtn = document.createElement('button');
 
-  h2.innerHTML = ttl;
-  p.innerHTML = cnt;
+  h2.innerHTML = toDoObj.title;
+  p.innerHTML = toDoObj.content;
+  deleteBtn.innerHTML = '削除';
+
+  deleteBtn.addEventListener('click', function () {
+    toDoArea.removeChild(li);
+
+    toDoItems.find(function () {
+
+    });
+  });
 
   var li = document.createElement('li');
 
   li.appendChild(h2);
   li.appendChild(p);
-  toDoArea.appendChild(li)
+  li.appendChild(deleteBtn);
+  toDoArea.appendChild(li);
+};
+
+
+//ローカルスレージからデータを取得
+function getLocalStorege() {
+  var data = localStorage.getItem('todo');
+  //JSONから配列に変換
+  parsedata = JSON.parse(data);
+  return parsedata;
 }
+
+function loadData() {
+  var toDoData = getLocalStorege();
+  if (!toDoData) {//dataが空（null）の場合
+    return false
+  }
+  for (var i = 0; i < toDoData.length; i++) {
+    var toDoItem = {
+      title: toDoData[i].title,
+      content: toDoData[i].content,
+    };
+    //27行目の配列が空の為、for分で作った物を代入
+    toDoItems.push(toDoItem);
+    displayToDo(toDoItem);
+  }
+}
+//リロードした時にtodoを表示
+  window.addEventListener('DOMContentLoaded', loadData);
+
+//toDoの一括削除
+  function removeToDo() {
+    while (toDoArea.firstChild) {
+      toDoArea.removeChild(toDoArea.firstChild);
+    }
+
+//ローカルストレージ一括削除
+    localStorage.clear();
+    toDoItems = [];
+  }
+
+removeBtn.addEventListener('click', removeToDo);
